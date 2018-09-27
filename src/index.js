3d89b5db1fc402d15afb945097458a88fbddc70c
Martinkevich Yana <Yana22685@yandex.ru>
2018-09-27 22:14:48 +0300

    6

--- src/index.js
+++ src/index.js
@@ -1,3 +1,73 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+
+ 	var temp = 0;
+	var t1 = 0;
+	var t2 = 0;
+	var arr = [];
+
+	while(str.length != 0) {
+
+		arr = func1(str, bracketsConfig);
+		t1 = arr[0];
+		t2 = arr[1];
+
+		temp = func(str, bracketsConfig, t1, t2);
+		if(temp == true) {
+			str = str.substring(0, t1) + str.substring(t1+1, t2) + str.substring(t2+1, str.length);
+		} else {
+			break;
+		}
+	}
+	
+	for(var j = 0; j < bracketsConfig.length; j++) {
+		if(bracketsConfig[j][0] == bracketsConfig[j][1]) {
+			return true;
+		}
+	}
+	if(str.length == 0) { return true; } else {
+		return false; }
 }
+
+ 	function func(str, bracketsConfig, t1, t2) {
+
+ 		var flag = false;
+ 		if(t2 != 0) {
+	 		for(var i = 0; i < bracketsConfig.length; i++) {
+				if((str[t1] + str[t2]) == (bracketsConfig[i][0] + bracketsConfig[i][1])) {
+					flag = true;
+				}
+			}
+		}
+		return flag;
+ 	}
+
+ 	function func1(str, bracketsConfig) {
+
+ 		var t1 = 0;
+ 		var t2 = 0;
+ 		var e = 1;
+ 		var ar = [];
+
+ 		for(var i = 0; i < str.length; i++) {
+			for(var j = 0; j < bracketsConfig.length; j++) {
+				if(str[i] == bracketsConfig[j][0]) {
+					t1 = i;
+				}
+			}
+		}
+
+		for(var i = t1 + 1; i < str.length; i++) {
+			if(t2 == 0) {
+				for(var j = 0; j < bracketsConfig.length; j++) {
+					if(str[i] == bracketsConfig[j][1]) {
+						t2 = i;					
+						break;
+					}
+				}
+			}
+		}
+		ar.push(t1, t2);
+		return ar;
+
+ 	}
